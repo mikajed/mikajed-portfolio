cc401129c2a47a5c2fb98ab6655735bc09e6e13e
mikajed <michaeljedrz@gmail.com>
2023-07-15 14:57:37 +0200

    js

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,13 @@
 const rotateObject = (element, angle, duration) => {
-    let currentAngle = 0;
-    const interval = setInterval(() => {
-      currentAngle += angle;
-      if (currentAngle > 360) {
-        currentAngle = 0;
-      }
-      element.style.transform = `rotate(${currentAngle}deg)`
-    }, duration);
-  };
+  let currentAngle = 0;
+  const interval = setInterval(() => {
+    currentAngle += angle;
+    if (currentAngle > 360) {
+      currentAngle = 0;
+    }
+    element.style.transform = `rotate(${currentAngle}deg)`
+  }, duration);
+};
   
-  const element = document.querySelector('.leftBottomCircle');
-  rotateObject(element, 1, 60);
-  
-
+const element = document.querySelector('.leftBottomCircle');
+rotateObject(element, 1, 60);
