fd958d92c20f49364f45fdd02277c022f8d8de79
mikajed <michaeljedrz@gmail.com>
2023-09-02 01:59:56 +0200

    add zeiten

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
-document.querySelector('.greeting').innerHTML = 'playing around';
+const jetzt = new Date();
+const stunde = jetzt.getHours();
+
+if (stunde >= 7 && stunde < 17) {
+    document.querySelector('.greeting').textContent = 'a software dev';
+} else {
+    document.querySelector('.greeting').textContent = 'having fun!';
+}
