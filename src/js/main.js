3f1ea0ab36ff1407d0e30c666dde90f73f4bad72
Michael <84669111+mikajed@users.noreply.github.com>
2023-10-06 15:19:47 +0200

    add rotating solar system in javascript

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,22 @@
-const jetzt = new Date();
-const stunde = jetzt.getHours();
+// const jetzt = new Date();
+// const stunde = jetzt.getHours();
 
-if (stunde >= 7 && stunde < 17) {
-    document.querySelector('.greeting').textContent = 'a software dev';
-} else {
-    document.querySelector('.greeting').textContent = 'having fun!';
+// if (stunde >= 7 && stunde < 17) {
+//     document.querySelector('.greeting').textContent = 'a software dev';
+// } else {
+//     document.querySelector('.greeting').textContent = 'having fun!';
+// }
+
+// solarsystem animate
+const solarSystemImage = document.querySelector('.solarsystem');
+
+let rotatingDegrees = 360;
+
+solarSystemImage.style.transform = `rotate(${rotatingDegrees}deg)`;
+
+function rotateSolarSystem() {
+  rotatingDegrees += 1;
+  solarSystemImage.style.transform = `rotate(${rotatingDegrees}deg)`;
 }
+
+setInterval(rotateSolarSystem, 50);
